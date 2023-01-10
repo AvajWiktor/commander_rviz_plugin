4f51f54e161903da69fb6f84643f9d67e2e69952
AvajWiktor <wiktor.markowski98@gmail.com>
2023-01-10 20:08:54 +0100

    Updated description

--- include/commander_rviz_plugins/commander_display.hpp
+++ include/commander_rviz_plugins/commander_display.hpp
@@ -1,17 +1,3 @@
-// Copyright 2022 Borong Yuan
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #pragma once
 
 #ifndef Q_MOC_RUN
@@ -70,7 +56,7 @@ namespace commander_rviz_plugins
         void publish_data(){publisher_->publish(*data_);}
         std::unique_ptr<rclcpp::Node> mainNode_ = std::make_unique<rclcpp::Node>("commander_gui_node");
 
-       rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_ = mainNode_->create_publisher<geometry_msgs::msg::Twist>("joy_teleop/cmd_vel", 10);
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_ = mainNode_->create_publisher<geometry_msgs::msg::Twist>("joy_teleop/cmd_vel", 10);
         std::unique_ptr<QPushButton> leftButton_;
         std::unique_ptr<QPushButton> rightButton_;
         std::unique_ptr<QPushButton> upButton_;
